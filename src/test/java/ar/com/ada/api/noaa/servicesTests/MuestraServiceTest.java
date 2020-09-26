783935a5f9df31827eced10c24b3375ca5afc316
Flor <florenciaromera@gmail.com>
2020-09-26 13:59:19 -0300

    - TestMethodOrder is used because the test are needed to be done in an specific order - @BeforeAll to setUp the variables that will be needed to run all the tests

--- src/test/java/ar/com/ada/api/noaa/servicesTests/MuestraServiceTest.java
+++ src/test/java/ar/com/ada/api/noaa/servicesTests/MuestraServiceTest.java
@@ -7,7 +7,13 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,11 +24,22 @@ import ar.com.ada.api.noaa.entities.Muestra;
 import ar.com.ada.api.noaa.repos.MuestraRepo;
 import ar.com.ada.api.noaa.services.MuestraService;
 
+/**
+ * TestMethodOrder is implemented bec the value of the variables
+ * used in one test are set for the next one
+ */
+@TestMethodOrder(OrderAnnotation.class)
+@TestInstance(Lifecycle.PER_CLASS)
 @SpringBootTest
 public class MuestraServiceTest {
     private final Boya BOYA = new Boya();
-
     private final LocalDateTime dt = LocalDateTime.of(2021, 2, 20, 21, 46, 31);
+
+    private Muestra m1;
+    private Muestra m2;
+
+    private ArrayList<Muestra> muestras;
+    private ArrayList<Muestra> ultima;
 
     @InjectMocks
     MuestraService mService;
@@ -30,13 +47,18 @@ public class MuestraServiceTest {
     @Mock
     MuestraRepo repo;
 
-    @Test
-    void getAnomaliaALERTAIMPACTO_SUCCESS() {
-        Muestra m1 = new Muestra(BOYA, 500.0, new Date(), 67.0, 120.0, "A34REW5");
-        Muestra m2 = new Muestra(BOYA, -300.0, new Date(), 23.0, 78.0, "J763F8");
-        ArrayList<Muestra> muestras = new ArrayList<>(List.of(m1, m2));
-        ArrayList<Muestra> ultima = new ArrayList<>(List.of(m2));
+    @BeforeAll
+    public void setUp() {
+        m1 = new Muestra(BOYA, 500.0, new Date(), 67.0, 120.0, "A34REW5");
+        m2 = new Muestra(BOYA, -300.0, new Date(), 23.0, 78.0, "J763F8");
+        muestras = new ArrayList<>(List.of(m1, m2));
+        ultima = new ArrayList<>(List.of(m2));
+    }
 
+    
+    @Test
+    @Order(1)
+    void getAnomaliaALERTAIMPACTO_SUCCESS() {
         when(repo.findMuestrasAbsolutasByBoyaId(1)).thenReturn(muestras);
         when(repo.ultimaMuestra(1)).thenReturn(ultima);
 
@@ -45,29 +67,31 @@ public class MuestraServiceTest {
     }
 
     @Test
+    @Order(2)
     void getAnomaliaKAIJUN_SUCCESS() {
-        Muestra m1 = new Muestra(BOYA, 230.0, new Date(), 67.0, 120.0, "A34REW5");
-        Muestra m2 = new Muestra(BOYA, -290.0, Date.from(dt.atZone(ZoneId.systemDefault()).toInstant()), 67.0, 120.0,
-                "A34REW5");
-        ArrayList<Muestra> muestrasNULL_ANOMALIA = new ArrayList<>(List.of(m1, m2));
-        ArrayList<Muestra> ultimaNULL_ANOMALIA = new ArrayList<>(List.of(m2));
+        m1.setAlturaNivelMar(230d);
+        m2.setAlturaNivelMar(-290d);
+        m2.setHorarioMuestra(Date.from(dt.atZone(ZoneId.systemDefault()).toInstant()));
+        m2.setLatitud(67d);
+        m2.setLongitud(120d);
 
-        when(repo.findMuestrasAbsolutasByBoyaId(1)).thenReturn(muestrasNULL_ANOMALIA);
-        when(repo.ultimaMuestra(1)).thenReturn(ultimaNULL_ANOMALIA);
+        when(repo.findMuestrasAbsolutasByBoyaId(1)).thenReturn(muestras);
+        when(repo.ultimaMuestra(1)).thenReturn(ultima);
 
         Optional<Anomalia> anomalia = mService.getAnomalia(1);
         assertEquals("ALERTA DE KAIJUN", anomalia.get().getTipoAlerta());
     }
-
+    /**
+     * The value of the m2 variables depends on the previous test 
+     */
     @Test
+    @Order(3)
     void getAnomaliaKAIJUN_FAILED() {
-        Muestra m1 = new Muestra(BOYA, 30.0, new Date(), 67.0, 120.0, "A34REW5");
-        Muestra m2 = new Muestra(BOYA, 90.0, new Date(), 67.0, 120.0, "A34REW5");
-        ArrayList<Muestra> muestrasNULL_ANOMALIA = new ArrayList<>(List.of(m1, m2));
-        ArrayList<Muestra> ultimaNULL_ANOMALIA = new ArrayList<>(List.of(m2));
-
-        when(repo.findMuestrasAbsolutasByBoyaId(1)).thenReturn(muestrasNULL_ANOMALIA);
-        when(repo.ultimaMuestra(1)).thenReturn(ultimaNULL_ANOMALIA);
+        m1.setAlturaNivelMar(30d);
+        m2.setAlturaNivelMar(90d);
+        
+        when(repo.findMuestrasAbsolutasByBoyaId(1)).thenReturn(muestras);
+        when(repo.ultimaMuestra(1)).thenReturn(ultima);
 
         Optional<Anomalia> anomalia = mService.getAnomalia(1);
         assertEquals(false, anomalia.isPresent());
